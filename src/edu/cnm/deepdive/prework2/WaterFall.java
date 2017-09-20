e54d9d297dc2e456af2afc60f4ea2c57f22f345a
caryl <carylstuart77@gmail.com>
2017-09-19 18:09:43 -0600

    Added fall method and added arguments.

--- src/edu/cnm/deepdive/prework2/WaterFall.java
+++ src/edu/cnm/deepdive/prework2/WaterFall.java
@@ -3,7 +3,9 @@ package edu.cnm.deepdive.prework2;
 public class WaterFall {
 
   public static void main(String[] args) {
-    System.out.println("Beautiful water feature.");
+      fall((args.length > 0) ? args[0] : "water fall");
+    }
+  private static void fall(String target) {
+    System.out.println("Beautiful " + target + "!");
   }
-
 }
